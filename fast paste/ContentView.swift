b87a206b396a71e1eb18dd6e8e086ca855de5d3d
hzzz <hzzz0823@gmail.com>
2023-04-11 21:11:32 +0800

    refactor

--- fast paste/ContentView.swift
+++ fast paste/ContentView.swift
@@ -8,16 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var clipboardEntries = ["Hello", "World"]
+    @State var clipboardEntries = ["Hello","Hello2", "World"]
     @State var lastChangeCount: Int = 0
     @State var searchText = ""
-    @State var selection: Int?
-    @State var selectedEntry: String? = "Hello"
-    
+    @State var selection: Int? = 0
     let pasteboard: NSPasteboard = .general
     
     var filteredClipboardEntries: [String] {
-        clipboardEntries.filter { searchText.isEmpty ? true : $0.localizedStandardContains(searchText) }
+        clipboardEntries.filter {
+            searchText.isEmpty ? true : $0.localizedStandardContains(searchText)
+        }
+        
     }
     
     var body: some View {
@@ -27,30 +28,14 @@ struct ContentView: View {
                 .padding()
             Divider()
             List(selection: $selection) {
-                ForEach(clipboardEntries.indices, id: \.self) { index in
-                    Text(clipboardEntries[index]).tag(index)
+                ForEach(filteredClipboardEntries.indices, id: \.self) { index in
+                    Text(filteredClipboardEntries[index]).tag(index)
                 }
             }
             .padding()
             .onAppear {
                 startTimer()
-                
-                NSEvent.addLocalMonitorForEvents(matching: [.keyDown]) { nsevent in
-                    print(nsevent.keyCode)
-                    if selection != nil {
-                        if nsevent.keyCode == 125 { // arrow down
-                            selection = selection! < clipboardEntries.count ? selection! + 1 : 0
-                        } else {
-                            if nsevent.keyCode == 126 { // arrow up
-                                selection = selection! > 1 ? selection! - 1 : 0
-                            }
-                        }
-                    } else {
-                        selection = 0
-                    }
-                    return nsevent
-                }
-                
+                addLocalMonitorForEvents()
             }
             .onReceive(NotificationCenter.default.publisher(for: NSApplication.didHideNotification)) { _ in
                 searchText = ""
@@ -59,6 +44,22 @@ struct ContentView: View {
         
     }
     
+    func addLocalMonitorForEvents() {
+        NSEvent.addLocalMonitorForEvents(matching: [.keyDown]) { nsevent in
+            if selection != nil {
+                if nsevent.keyCode == 125 { // arrow down
+                    selection = selection! < filteredClipboardEntries.count - 1 ? selection! + 1 : selection!
+                } else {
+                    if nsevent.keyCode == 126 { // arrow up
+                        selection = selection! > 0 ? selection! - 1 : selection!
+                    }
+                }
+            } else {
+                selection = 0
+            }
+            return nsevent
+        }
+    }
     func startTimer() {
         Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true) { timer in
             if self.lastChangeCount != self.pasteboard.changeCount {
