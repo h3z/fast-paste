8495f872d33f0c744a9d1a51c0d8f9404d518955
hzzz <hzzz0823@gmail.com>
2023-04-11 12:58:45 +0800

    Initial Commit

---
+++ fast paste/fast_pasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  fast_pasteApp.swift
+//  fast paste
+//
+//  Created by hzzz on 2023/4/11.
+//
+
+import SwiftUI
+
+@main
+struct fast_pasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
